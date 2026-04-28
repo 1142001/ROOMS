16bde9ef767475a66187cdd1449813f633b7a334
Siddheshwari Dilip Sonar <siddheshwaridsonar@gmail.com>
2026-04-28 17:10:47 +0530

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,21 +9,21 @@ export const loginUser = (data) => axios.post(`${API}/auth/login`, data);
 // ROOMS
 export const getRooms = (fromDate, toDate) =>
   axios.get(`${API}/rooms`, {
-    params: { fromDate, toDate }
+    params: { fromDate, toDate },
   });
 
 export const createRoom = (data, token) =>
   axios.post(`${API}/rooms`, data, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
 
 export const deleteRoom = (id, token) =>
   axios.delete(`${API}/rooms/${id}`, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
 
 // BOOKINGS
 export const bookRoom = (data, token) =>
   axios.post(`${API}/bookings`, data, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `Bearer ${token}` },
   });
